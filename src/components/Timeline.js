abe1f46f74c1e9dfbc5cb09093e914f86620fcde
Qazi Maaz Arshad <qarshad@informatica.com>
2023-04-22 22:53:48 +0530

    Added KT

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,6 +2,7 @@ import React from 'react';
 import sunil from '../assets/sunil.png';
 import fun_friday from '../assets/fun_friday.png';
 import jenkins from '../assets/jenkins.png';
+import kt from '../assets/kt.png';
 const Timeline = () => {
     return (
         <div>
@@ -11,6 +12,12 @@ const Timeline = () => {
                         <div className="info">
                             <h3 className="title" data-aos="fade-left">Worked during the company holidays <span>Customer First Approach</span></h3>
                             <img src={sunil} alt="" data-aos="fade-left" />
+                        </div>
+                    </div>
+                    <div className="timeline_card">
+                        <div className="info">
+                            <h3 className="title" data-aos="fade-right">Delivered a KT Session on <span>Spring Boot Basics</span></h3>
+                            <img src={kt} alt="" data-aos="fade-right" />
                         </div>
                     </div>
                     <div className="timeline_card">
