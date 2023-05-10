766295bf4785c6d26f087c4b26db83246eedf3e0
Qazi Maaz Arshad <qarshad@informatica.com>
2023-05-10 16:30:11 +0530

    Added SHM KT

--- src/components/Extras.js
+++ src/components/Extras.js
@@ -3,6 +3,7 @@ import sunil from "../assets/sunil.png";
 import fun_friday from "../assets/fun_friday.png";
 import jenkins from "../assets/jenkins.png";
 import kt from "../assets/kt.png";
+import shm_kt from "../assets/shm_kt.png";
 const Timeline = () => {
     return (
         <div>
@@ -36,9 +37,17 @@ const Timeline = () => {
                     <div className="timeline_card">
                         <div className="info">
                             <h3 className="title" data-aos="fade-right">
+                                Facilitated an enablement session on <span>SHM UI</span>
+                            </h3>
+                            <img src={shm_kt} alt="" data-aos="fade-right" />
+                        </div>
+                    </div>
+                    <div className="timeline_card">
+                        <div className="info">
+                            <h3 className="title" data-aos="fade-left">
                                 Organized a team bonding activity <span>Fun Friday</span>
                             </h3>
-                            <img src={fun_friday} alt="" data-aos="fade-right" />
+                            <img src={fun_friday} alt="" data-aos="fade-left" />
                         </div>
                     </div>
                 </div>
