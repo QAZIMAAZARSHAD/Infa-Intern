d8799ea1896c779a28cd8d56468b3823759d3d1e
Qazi Maaz Arshad <qarshad@informatica.com>
2023-06-12 15:14:50 +0530

    update pr

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -48,7 +48,7 @@ const Introduction = () => {
                             <p>JIRA Issues</p>
                         </div>
                         <div className="intro_cards_large">
-                            <p>44</p>
+                            <p>45</p>
                             <p>Pull Requests Merged</p>
                         </div>
                         <div className="intro_cards_large">
